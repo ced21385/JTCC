c95fec79b25d25bbd502a1733f8c7eda333f9cfb
ced21385 <ced21385@email.vccs.edu>
2019-03-07 16:19:08 -0500

    Delete InvalidRadiusException.java

--- src/InvalidRadiusException.java
+++
@@ -1,9 +0,0 @@
-package chapter12;
-
-import java.lang.Exception;
-
-public class InvalidRadiusException extends Exception {  // checked exception
-
-    public InvalidRadiusException(double radius) {super("Invalid radius " + radius);}
-
-}  // class InvalidRadiusException
