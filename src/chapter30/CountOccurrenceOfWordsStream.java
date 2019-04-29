733cc70e42af21b760944a8d47707716b37c1ba6
ced21385 <ced21385@email.vccs.edu>
2019-04-29 18:38:31 -0400

    almost done

---
+++ src/chapter30/CountOccurrenceOfWordsStream.java
@@ -0,0 +1,25 @@
+package chapter30;
+
+import java.lang.System;
+import java.lang.String;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CountOccurrenceOfWordsStream {
+
+    public static void main(String[] args) {
+
+        String text = "Good morning. Have a good class. Have a good visit. Have fun!";
+
+        Stream.of(text.split("[\\s+\\p{Punct}]")).
+                parallel().
+                filter(e -> e.length() > 0).
+                collect(
+                        Collectors.groupingBy(String::toLowerCase, TreeMap::new, Collectors.counting())
+                ).
+                forEach((k, v) -> System.out.println(k + " " + v));
+
+    }  // main()
+
+}  // class CountOccurenceOfWordsStream
